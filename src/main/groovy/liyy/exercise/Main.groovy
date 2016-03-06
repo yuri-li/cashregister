f98e04985334f7e788970dc1c2a9c93841d4d631
yurilee <javaliyuanyuan@163.com>
2016-03-06 14:30:32 +0800

    修改测试消息的打印内容

--- src/main/groovy/liyy/exercise/Main.groovy
+++ src/main/groovy/liyy/exercise/Main.groovy
@@ -8,8 +8,9 @@ import liyy.exercise.controller.Strategy
  * 打印测试结果到控制台，或打开最后一行的注释，输出到文件中
  */
 StringBuffer buffer = new StringBuffer()
+CashRegister cash = new CashRegister()
 Strategy.values().each {
-    buffer << it.descrption << "\n"  << CashRegister.getMessage(it) << "====================分割线============================\n"
+    buffer << it.description << "\n"  << cash.getMessage(it)
 }
 println buffer.toString()
-//new File("cashregister.txt") << buffer.toString()
+//new File("小票.txt") << buffer.toString()
