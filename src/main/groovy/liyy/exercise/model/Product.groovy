66f11ab4a600410439f4d10bcf37555bd361dd46
yurilee <javaliyuanyuan@163.com>
2016-03-06 13:53:26 +0800

    修改小票的消息模板后，重构商品属性构建流程（去掉不参与活动的商品，构建小票格式的硬编码。将其迁移至PromotionEnum）

--- src/main/groovy/liyy/exercise/model/Product.groovy
+++ src/main/groovy/liyy/exercise/model/Product.groovy
@@ -17,19 +17,21 @@ class Product {
    Integer realNumber //赠送活动的产品，需要在总数量中去掉赠送的产品的数量，才去计算商品总价
    Integer freeNumber //赠送产品的数量
    Double total //总价
-   Double saveMoney //节省的钱
+   double saveMoney //节省的钱
    String shoppingListMessage //购物清单
    String freeMessage //买二赠一，赠送物品的清单
-   AvilablePromotionEnum promotion //享受的优惠活动
+   PromotionEnum promotion //享受的优惠活动
+   private static final Map products = ["ITEM000001":["id":"ITEM000001","name":"可口可乐","quantifier":"瓶","price":3.00],
+                           "ITEM000002":["id":"ITEM000002","name":"羽毛球","quantifier":"个","price":1.00],
+                           "ITEM000003":["id":"ITEM000003","name":"苹果","quantifier":"斤","price":5.50]]
 
    private Product(){}
    /**
-    * 根据code，获得相应的产品
+    * 根据条形码，获得产品编号、数量
     * @param code
     */
    static Product getProduct(String code){
       assert code : "商品条形码不允许为空"
-      def product
       String tempId
       int tempNumber
       String[] arr = code.split("-")
@@ -41,21 +43,15 @@ class Product {
          tempNumber = 1
       }
       assert tempId : "截取ID失败，请检查输入的编码格式：${code}"
-      //实际项目中，根据ID，从数据库中查找相应产品的默认属性
-      switch(tempId){
-         case "ITEM000001":
-            product = new Product(id:tempId,name:"可口可乐",quantifier:"瓶",price:3.00,number:tempNumber)
-            break;
-         case "ITEM000002":
-            product = new Product(id:tempId,name:"羽毛球",quantifier:"个",price:1.00,number:tempNumber)
-            break;
-         case "ITEM000003":
-            product = new Product(id:tempId,name:"苹果",quantifier:"斤",price:5.50,number:tempNumber)
-            break;
-      }
+
+      Product product = products.get(tempId) as Product
+      product.number = tempNumber
       return product
    }
-   void rebuildProduct(){
+   /**
+    * 汇总完商品后，补全小计、节省金额等
+    */
+   void rebuild(){
       Map params = promotion?.params
       params?.each {
          String key = it.key
@@ -68,10 +64,6 @@ class Product {
          }else{
             this."${key}" = text
          }
-      }
-      if(!promotion){
-         this.total = this.number * this.price
-         this.shoppingListMessage = "名称：${name}，数量：${number}${quantifier}，单价：${price}(元)，小计：${total}(元)"
       }
    }
 }
