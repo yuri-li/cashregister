a43dcaaaf695f5a7bf783195e51fd8dd1c2319fb
yurilee <javaliyuanyuan@163.com>
2016-03-06 13:44:41 +0800

    添加不参加任何活动时，商品打印的小票格式

--- src/main/groovy/liyy/exercise/model/PromotionEnum.groovy
+++ src/main/groovy/liyy/exercise/model/PromotionEnum.groovy
@@ -2,10 +2,12 @@ package liyy.exercise.model
 
 /**
  * Created by Yuri on 2016/3/2.<br/>
- * 枚举当前所有正在生效的活动，比如打折，是95折，还是98折等<br/>
- * 约定：规则的ordinal为其优先级。即：“买二赠一”的优先级>“打95折”
+ * 枚举所有活动，比如打折、买二赠一等<br/>
+ * 约定：
+ * 1、规则的ordinal为其优先级。即：“买二赠一”的优先级>“打95折”
+ * 2、产品的属性，与属性“params”的键必须相同。
  */
-enum AvilablePromotionEnum {
+enum PromotionEnum {
     //total:优惠后的总花费,saveMoney:节省的钱,shoppingListMessage:购物清单的信息
     _95Discount("打95折",["total":'${product.number}*${product.price}*0.95',
                                     "saveMoney":'${product.number}*${product.price}*0.05',
@@ -16,12 +18,13 @@ enum AvilablePromotionEnum {
                                     "total":'${product.realNumber}*${product.price}',
                                     "saveMoney":'${product.freeNumber}*${product.price}',
                                     "shoppingListMessage":'名称：${product.name}，数量：${product.number}${product.quantifier}，单价：${product.price}(元)，小计：${product.total}(元)',
-                                    "freeMessage":'名称：${product.name}，数量：${product.freeNumber}${product.quantifier}'])
+                                    "freeMessage":'名称：${product.name}，数量：${product.freeNumber}${product.quantifier}']),
+    none("不享受优惠活动",["total":'${product.number}*${product.price}',"shoppingListMessage":'名称：${product.name}，数量：${product.number}${product.quantifier}，单价：${product.price}(元)，小计：${product.total}(元)'])
 
-    String promotion//属于哪一种优惠活动
+    String description//优惠活动
     Map params //列出与促销活动相关的参数与表达式
-    AvilablePromotionEnum(promotion,params){
-        this.promotion = promotion
+    PromotionEnum(description, params){
+        this.description = description
         this.params = params
     }
 }
